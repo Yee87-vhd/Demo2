6b46e90f7de07621de2f0a255b200d80b946c422
Yee87 <tompoo.2015@gmail.com>
2022-01-19 16:01:40 +0800

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by 凌翊瀗 on 2022/1/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by 凌翊瀗 on 2022/1/19.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
